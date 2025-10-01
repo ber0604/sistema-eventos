79dd864006e9e8280dab14ca5156f025d50008d2
Bernardo Marquetti <bernardofaccini6@gmail.com>
2025-10-01 19:23:03 -0300

    adicionando swagger

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 require("dotenv").config(); // Carrega variáveis de ambiente
 const app = require("./src/app");
 const createAdminIfNotExists = require("./src/utils/adminUser.js");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+// Cria admin se não existir
 createAdminIfNotExists();
+
+// Carrega o arquivo Swagger
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// Rota para documentação Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
 });
